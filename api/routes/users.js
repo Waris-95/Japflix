5a7ba849245cfca031b1c11ad82dde8919eb5f22
Abdul Waris <maiyaleeg223@gmail.com>
2024-03-02 00:06:35 -0500

    delete route added

--- api/routes/users.js
+++ api/routes/users.js
@@ -5,28 +5,70 @@ const verify  = require("../verifyTokens");
 
 //UPDATE 
 router.put("/:id", verify, async (req, res)=> {
-    if (req.user.id === req.params.id || req.user.isAdmin){
-        if(req.body.password) {
-            req.body.password = CryptoJS.AES.encrypt(
-                req.body.password,
-                 process.env.SECRET_KEY
-                 ).toString();
+    if (req.user.id === req.params.id || req.user.isAdmin) {
+        if (req.body.password) {
+          req.body.password = CryptoJS.AES.encrypt(
+            req.body.password,
+            process.env.SECRET_KEY
+          ).toString();
         }
         try {
-            const updatedUser = await User.findByIdAndUpdate(req.params.id, {
-                $set:req.body,
-            });
-            res.status(200).json(updatedUser);
-        } catch(err){
-            res.status(500).json(err)
+          const updatedUser = await User.findByIdAndUpdate(
+            req.params.id,
+            {
+              $set: req.body,
+            },
+            { new: true }
+          );
+          res.status(200).json(updatedUser);
+        } catch (err) {
+          res.status(500).json(err);
+        }
+      } else {
+        res.status(403).json("Not authorized to take this action");
+      }
+    });
+
+//DELETE
+router.delete("/find/:id", verify, async (req, res) => {
+    if (req.user.id === req.params.id || req.user.isAdmin) {
+      try {
+        await User.findByIdAndDelete(req.params.id);
+        res.status(200).json("Sucessfully Deleted");
+      } catch (err) {
+        res.status(500).json(err);
+      }
+    } else {
+      res.status(403).json("Not authorized to take this action");
+    }
+  });
+
+//GET
+router.get("/", verify, async (req, res)=> {
+    try{
+        const user = await User.findById(req.params.id);
+        const { password, ...info} = user._doc;
+        res.status(200).json(info);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+//GET ALL
+router.get("/", verify, async (req, res)=> {
+    const query = req.query.new;
+    if (req.user.isAdmin) {
+        try {
+            const users = query ? await User.find().limit(10) : await User.find();
+            res.status(200).json(users);
+    } catch(err) {
+        res.status(500).json(err);
         }
     } else {
         res.status(403).json("Not authorized to take this action");
     }
-})
-//DELETE
-//GET
-//GET ALL
+});
+
 //GET USER STATS
 
 module.exports = router;
--- api/verifyTokens.js
+++ api/verifyTokens.js
@@ -1,18 +1,20 @@
 const jwt = require("jsonwebtoken");
 
-function verify(req, res, next) {
-  const authHeader = req.headers.token;
-  if (authHeader) {
-    const token = authHeader.split(" ")[1];
+function verify (req, res, next) {
+    const authHeader = req.headers.token;
+    if(authHeader){
+        const token = authHeader.split(" ")[1];
 
-    jwt.verify(token, process.env.SECRET_KEY, (err, user) => {
-      if (err) res.status(403).json("Token is not valid!");
-      req.user = user;
-      next();
-    });
-  } else {
-    return res.status(401).json("You are not authenticated!");
-  }
+        jwt.verify(token, process.env.SECRET_KEY, (err, user)=>{
+            if(err) res.status(403).json("Invalid Token");
+            
+            
+            req.user = user
+            next();
+        })
+    } else {
+        return res.status(401).json("Not authorized to take this action");
+    }
 }
 
 module.exports = verify;
