d2ed4f12f6299d86539250bfa40affed986dcb91
Abdul Waris <maiyaleeg223@gmail.com>
2024-03-05 23:26:32 -0500

    Updated .gitignore

--- api/routes/movies.js
+++ api/routes/movies.js
@@ -3,6 +3,16 @@ const Movie = require("../models/Movie");
 const verify = require("../verifyTokens");
 
 //CREATE
+router.post("/", verify, async (req, res)=> {
+    if (req.user.isAdmin) {
+        const newMovie = new Movie(req.body);
+        try {
+
+        } catch (err) {
+            res.status(500).json(err);
+        }
+    }
+})
 
 //UPDATE
 
