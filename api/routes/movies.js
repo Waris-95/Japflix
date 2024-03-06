298058e29b17222800e38c95ff7313f67d38e55e
Abdul Waris <maiyaleeg223@gmail.com>
2024-03-06 00:43:40 -0500

    Adding crud operations

--- api/routes/movies.js
+++ api/routes/movies.js
@@ -3,25 +3,64 @@ const Movie = require("../models/Movie");
 const verify = require("../verifyTokens");
 
 //CREATE
-router.post("/", verify, async (req, res)=> {
+router.post("/", verify, async (req, res) => {
     if (req.user.isAdmin) {
-        const newMovie = new Movie(req.body);
-        try {
-            const savedMovie = await newMovie.save();
-            res.status(201).json(savedMovie);
-        } catch (err) {
-            res.status(500).json(err);
-        }
+      const newMovie = new Movie(req.body);
+      try {
+        const savedMovie = await newMovie.save();
+        res.status(201).json(savedMovie);
+      } catch (err) {
+        res.status(500).json(err);
+      }
     } else {
-        res.status(403).json("Not authorized to take this action");
+      res.status(403).json("Not authorized to take this action");
     }
-})
-
-//UPDATE
-
-//DELETE
-
-//GET
+  });
+  
+  //UPDATE
+  router.put("/:id", verify, async (req, res) => {
+    if (req.user.isAdmin) {
+      try {
+        const updatedMovie = await Movie.findByIdAndUpdate(
+          req.params.id,
+          {
+            $set: req.body,
+          },
+          { new: true }
+        );
+        res.status(200).json(updatedMovie);
+      } catch (err) {
+        res.status(500).json(err);
+      }
+    } else {
+      res.status(403).json("Not authorized to take this action");
+    }
+  });
+  
+  //DELETE
+  router.delete("/:id", verify, async (req, res) => {
+    if (req.user.isAdmin) {
+      try {
+        await Movie.findByIdAndDelete(req.params.id);
+        res.status(200).json("Successfully Deleted");
+      } catch (err) {
+        res.status(500).json(err);
+      }
+    } else {
+      res.status(403).json("Not authorized to take this action");
+    }
+  });
+  
+  //GET
+  router.get("/find/:id", verify, async (req, res) => {
+    try {
+      const movie = await Movie.findById(req.params.id);
+      res.status(200).json(movie);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  });
+  
 
 //GET RANDOM
 
