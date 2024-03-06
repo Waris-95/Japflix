d5f43cb53486f2bce4d8284d040ff9d257baf511
Abdul Waris <maiyaleeg223@gmail.com>
2024-03-06 00:07:08 -0500

    Post route added for movies

--- api/routes/movies.js
+++ api/routes/movies.js
@@ -7,10 +7,13 @@ router.post("/", verify, async (req, res)=> {
     if (req.user.isAdmin) {
         const newMovie = new Movie(req.body);
         try {
-
+            const savedMovie = await newMovie.save();
+            res.status(201).json(savedMovie);
         } catch (err) {
             res.status(500).json(err);
         }
+    } else {
+        res.status(403).json("Not authorized to take this action");
     }
 })
 
