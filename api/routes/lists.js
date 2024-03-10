58e2320bb579f4614910da6075ce731037833db2
Abdul Waris <maiyaleeg223@gmail.com>
2024-03-09 21:33:31 -0500

    lalal

--- api/routes/lists.js
+++ api/routes/lists.js
@@ -3,7 +3,6 @@ const List = require("../models/List");
 const verify = require("../verifyToken");
 
 //CREATE
-
 router.post("/", verify, async (req, res) => {
   if (req.user.isAdmin) {
     const newList = new List(req.body);
@@ -19,9 +18,28 @@ router.post("/", verify, async (req, res) => {
 });
 
 //DELETE
-
+router.delete("/:id", verify, async (req, res) =>  {
+    if (req.user.isAdmin) {
+        try {
+            await List.findByIdAndDelete(req.params.id);
+            res.status(201).json("Deletion Successful");
+        } catch (err) {
+            res.status(500).json(err);
+        }
+    }
+})
 
 //GET
+router.get("/", verify, async (req, res) => {
+    const typeQuery = req.query.type;
+    const genreQuery = req.query.genre;
+    let list = [];
+    try {
+        
+    } catch (err) {
+        res.status(500).json(err);
+    }
+})
 
 
 module.exports = router;
